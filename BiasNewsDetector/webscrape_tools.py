367c92475fcd052d3daf673c5c8c06299eb30cdc
jordanshaheen <jordan.shaheen@icloud.com>
2024-03-19 20:29:59 -0400

    added sentiment score adjustment and cleaned up the output a little.

--- BiasNewsDetector/webscrape_tools.py
+++ BiasNewsDetector/webscrape_tools.py
@@ -1,4 +1,5 @@
 from newspaper import Article
+from bs4 import BeautifulSoup
 
 
 def newspaper_scrape(url):
@@ -12,3 +13,28 @@ def newspaper_scrape(url):
         return title, text, len(words)
     except Exception as e:
         return f"An error occurred: {e}", 0
+
+
+def newspaper_scrape2(url):
+    try:
+        # Download and parse the article
+        article = Article(url)
+        article.download()
+        article.parse()
+
+        # Get the raw HTML and parse it with BeautifulSoup
+        soup = BeautifulSoup(article.html, 'html.parser')
+
+        # Find all paragraph tags and get the text content
+        paragraphs = soup.find_all('p')
+        paragraphs_text = [paragraph.get_text() for paragraph in paragraphs if paragraph.get_text().strip() != '']
+
+        # Join paragraphs with newline characters to maintain spacing
+        text_by_paragraphs = '\n\n'.join(paragraphs_text)
+
+        title = article.title
+        words = text_by_paragraphs.split()
+
+        return title, text_by_paragraphs, len(words)
+    except Exception as e:
+        return f"An error occurred: {e}", '', 0
