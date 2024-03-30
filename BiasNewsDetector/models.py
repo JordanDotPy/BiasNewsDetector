62476a71827f1a2f6075d22bdd7a6262e81fac9e
Alexander Hatton <ahattonbm@gmail.com>
2024-03-30 19:52:35 -0400

    Added loading message

--- BiasNewsDetector/models.py
+++ BiasNewsDetector/models.py
@@ -5,8 +5,3 @@ class NewsArticle(models.Model):
   url = models.URLField(max_length=200)
   title = models.CharField(max_length=200)
   content = models.CharField(max_length=10000)
-
-"""
-class ArticleParseTime(models.Model):
-  seconds_per_word = models.DecimalField(max_digits = 5, decimal_places = 4)
-"""
