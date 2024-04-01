0a1b208e92bf72cf1d5c1e263af6d1c442fd8302
Alexander Hatton <ahattonbm@gmail.com>
2024-04-01 19:46:46 -0400

    added sentiment error handling

--- BiasNewsDetector/views.py
+++ BiasNewsDetector/views.py
@@ -37,8 +37,11 @@ def process_article(request):
         if newspaper_words < 0:
             return error_handler(request, newspaper_title)
 
+        try:
         # Find all named entities within the article and provide sentiment analysis
-        p_sentence, neg_sentence, neu_sentence, ent_sentence, quoted_sentences, all_sentences = full_article_sentiment_analysis(newspaper_text, newspaper_title)
+            p_sentence, neg_sentence, neu_sentence, ent_sentence, quoted_sentences, all_sentences = full_article_sentiment_analysis(newspaper_text, newspaper_title)
+        except Exception as e:
+            return error_handler(request, e)
         print("=====POSITIVE SENTENCES=====")
         print(p_sentence)
         print("=====NEGATIVE SENTENCES=====")
